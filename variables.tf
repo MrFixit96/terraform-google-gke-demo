cf961bdd6a31fa3141af48b48354e672f458c62f
James Anderton <james@janderton.com>
2020-09-04 18:30:58 -0500

    adding auto.tfvars and added default network/subnet to vars.tf

--- variables.tf
+++ variables.tf
@@ -2,8 +2,12 @@ variable "env" {}
 variable "project_id" {}
 variable "cluster_region" {}
 variable "cluster_name" {}
-variable "network_name" {}
-variable "subnetwork_name" {}
+variable "network_name" {
+  default = "default"
+}
+variable "subnetwork_name" {
+  default = "default"
+}
 variable "node_labels" {}
 variable "node_tags" {}
 variable "default_node_pool" {}
