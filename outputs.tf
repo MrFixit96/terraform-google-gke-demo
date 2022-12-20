a5ae3ec622b532149f79498803f4c79f7fa03ba9
MrFixit96 <james@janderton.com>
2022-12-20 13:38:53 -0600

    fixing outputs

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 output "cluster_endpoint" {
   description = "The IP address of the cluster master."
   sensitive   = true
-  value       = google_container_cluster.commons-gke.endpoint
+  value       = module.google_container_cluster.commons-gke.endpoint
 }
